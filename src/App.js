b98580e41d3894e6391085715efc7642e1e18c8d
abrarfuad27 <abrarfuad51@gmail.com>
2023-09-30 13:57:54 -0400

    edit font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
+
 
 function App() {
   return (
     <div>
-      <h1>Hello </h1>
+      <h1>Helloy </h1>
     </div>
   );
 }
