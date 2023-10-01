59601e66add81cc72a649e367caec12ef7d49f15
abrarfuad27 <abrarfuad51@gmail.com>
2023-10-01 11:47:28 -0400

    updated graphics

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import "./style.css";
 import NameCard from "./components/NameCard";
 
 const data = [
-  { name: "Abrar", description: "I study CS.", technology: ["Python", "Java"] },
-  { name: "Abrar", description: "I study CS.", technology: ["Python", "Java"] },
-  { name: "Abrar", description: "I study CS.", technology: ["Python", "Java"] },
-  { name: "Abrar", description: "I study CS.", technology: ["Python", "Java"] },
+  { name: "Mark", description: "Hi I am Mark. I am a U3 Neuroscience major with a CS minor.", technology: ["Python", "Java","Flask","Bootstrap"] },
+  { name: "Katie", description: "hello everyone! I'm Katie and I am looking for a team as well", technology: ["TensorFlow","Keras","Django","React","Node","Typescript"] },
+  { name: "Jeremy", description: "My name is Jeremy and I am searching for someone who has worked with ML before ", technology: ["UI/UX","Java","Kotlin"] },
 ];
 
 function App() {
